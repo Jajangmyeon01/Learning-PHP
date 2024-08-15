874749b7c905f0bf4795e332c272f3d75cf76da1
Gen Exus Vergara <121187643+Jajangmyeon01@users.noreply.github.com>
2024-08-14 19:14:33 -0700

    Add files via upload

---
+++ PHP/2. Syntax/index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Syntax</title>
+    </head>
+    <body>
+        <?php
+            // Sample single-line comment //
+            /*
+                Sample multi-line comment
+            */
+
+            echo "comment section"; 
+        ?>
+         
+        <?php if (true) { ?>
+            <p>Do this...</p>
+        <?php } ?>
+    </body>
+</html>
